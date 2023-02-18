9c5fdcd3084e44746e9ac14492119e04adce71c2
lewis <lcb005@uark.edu>
2023-02-18 01:58:27 -0600

    added timeline to work-experience section + fixed some responsive design issues with toc section + added jquery + reworded some position titles

--- index.js
+++ index.js
@@ -1,46 +1,113 @@
-var ticks = 0;
-var ticksRequired = 35;
-
-function spotlightMouseMove(event)
+// Defining some helper functions
+function getMonthNumberFromName(monthName)
 {
-    if(event.target !== event.currentTarget) return;
+    return new Date(`${monthName} 1, 2023`).getMonth();
+}
 
-    if(ticks < ticksRequired)
-    {
-        ticks++;
-        return;
+
+// Extract all date strings to aray
+var dates = [];
+var currentMonth = new Date().getMonth();
+var currentYear = new Date().getFullYear();
+$("div#work-experience-cards div.card-header").each(function () {
+    var headerString = $(this).text().trim();
+    var headerBits = headerString.split(" ");
+    var startMonth = getMonthNumberFromName(headerBits[0]);
+    var startYear = parseInt(headerBits[1]);
+    var endMonth;
+    var endYear;
+
+    // Conditionally handle "Present" if specified
+    if (headerBits[3].toLowerCase() == "present") {
+        endMonth = currentMonth;
+        endYear = currentYear;
+    }
+    else {
+        endMonth = getMonthNumberFromName(headerBits[3]);
+        endYear = parseInt(headerBits[4]);
     }
 
-    ticks = 0;
+    // Get title from sibling element
+    var title = $(this).siblings("div.card-body").children(".card-title").text().trim();
 
-    var x = event.offsetX;
-    var y = event.offsetY;
+    dates.push([
+        startMonth,
+        startYear,
+        endMonth,
+        endYear,
+        title
+    ]);
+});
 
-    var width = event.target.offsetWidth;
-    var height = event.target.offsetHeight;
 
-    var widthMiddle = width / 2;
-    var heightMiddle = height / 2;
-
-    var normalizedX = (widthMiddle - x) / width;
-    var normalizedY = (heightMiddle - y) / height;
-
-    var boxShadowX = 20 * normalizedX;
-    var boxShadowY = 20 * normalizedY;
-
-    //var color = getComputedStyle(event.target)["border-left-color"];
-    var color = '#bdc3c7';
-    var boxShadowString = `${boxShadowX}px ${boxShadowY}px 20px ${color}`;
-
-    event.target.style.boxShadow = boxShadowString;
-
-    console.log(normalizedX, normalizedY);
-}
-
-var elements = document.getElementsByClassName("spotlight");
-
-for(var element of elements)
+// Empty work experience timeline
+function emptyWorkExperienceTimeline()
 {
-    element.addEventListener("mousemove", spotlightMouseMove);
-    console.log(element);
+    $("div#work-experience-timeline-header").empty();
+    $("div#work-experience-timeline > a").remove()
 }
+
+
+// Populating work experience timeline
+function populateWorkExperienceTimeline()
+{
+    // Timeline math to determine resolution
+    var smallestYear = dates[dates.length - 1][1];
+    var largestYear = dates[0][3];
+    var totalYearDelta = largestYear - smallestYear;
+    var totalMonthCount = (totalYearDelta * 12) + 12;
+    var timelineWidth = $("div#work-experience-timeline").width();
+    var timelineResolution = timelineWidth / totalMonthCount;
+    var timelineElementHeight = 40;
+
+    // Add timeline element for each date
+    for (var z = 0; z < dates.length; z++)
+    {
+        var date = dates[z];
+        var startYearDelta = date[1] - smallestYear;
+        var endYearDelta = largestYear - date[3];
+        var leftCount = (startYearDelta * 12) + date[0];
+        var rightCount = (endYearDelta * 12) + (12 - date[2]);
+        var leftSpacing = leftCount * timelineResolution;
+        var rightSpacing = rightCount * timelineResolution;
+        var elementWidth = timelineWidth - leftSpacing - rightSpacing
+
+        var timelineLink = $("<a></a>")
+            .attr("href", `#work-experience-${z}`)
+            .css("display", "block")
+            .css("width", `${elementWidth}px`)
+            .css("height", `${timelineElementHeight}px`)
+            .css("margin-left", `${leftSpacing}px`)
+            .css("margin-right", `${rightSpacing}px`)
+            .addClass("auto-fade")
+            .attr("title", date[4]);
+
+        $("div#work-experience-timeline").append(timelineLink);
+    }
+
+
+    // Add timeline headers for each year
+    for (var z = smallestYear; z < largestYear + 1; z++)
+    {
+        var yearDelta = largestYear - smallestYear + 1;
+        var elementWidth = timelineWidth / yearDelta;
+        var headerText;
+
+        // Limit the year to last two characters if width sufficiently small
+        var headerText = (elementWidth < 55) ? z % 100 : z;
+
+        var timelineHeader = $("<div></div>")
+            .css("width", `${elementWidth}px`)
+            .text(headerText);
+
+        $("div#work-experience-timeline-header").append(timelineHeader);
+    }
+}
+
+$(window).on("resize", function()
+{
+    emptyWorkExperienceTimeline();
+    populateWorkExperienceTimeline();
+});
+
+populateWorkExperienceTimeline();
